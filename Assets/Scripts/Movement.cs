cfd35a5e6b8da21d5bf38081ba370b4db9921972
r3laty <lolkers.ilya@gmail.com>
2023-12-20 18:45:44 +0300

    Make cut scene and dialogue

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,14 +1,19 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rotationSpeed = 400f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float rotationSpeed = 400f;
+    [SerializeField] private Transform cameraTransform;
 
     private CharacterController _characterController;
+    private Rigidbody _rb;
 
     void Start()
     {
+        _rb = GetComponent<Rigidbody>();
         _characterController = GetComponent<CharacterController>();
     }
 
@@ -22,13 +27,15 @@ public class Movement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
 
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, 0.1f);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,
+                    targetAngle, ref rotationSpeed, 0.1f);
 
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f)
+                     * Vector3.forward;
 
             _characterController.Move(moveDirection * speed * Time.fixedDeltaTime);
         }
